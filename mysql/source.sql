dbb661a8104fd0493d763d1c939b2808d3dff1e8
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2024-01-31 12:56:03 +0000

    SELECT ORDER BY

--- mysql/source.sql
+++ mysql/source.sql
@@ -10,3 +10,9 @@ source /home/ubuntu/bookstore-data.sql
  SELECT * FROM staff;
 /*SELECTで必要なカラムだけ取得*/
  SELECT book_id, title, year FROM book;
+ 
+ 
+SELECT * FROM book ORDER BY year;
+SELECT * FROM book ORDER BY year DESC;
+SELECT * FROM book ORDER BY year DESC;
+SELECT * FROM book ORDER BY year, price;
