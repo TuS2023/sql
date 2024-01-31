817498971ab868ae94f30851acdc9cb4d043411d
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2024-01-31 11:23:10 +0000

    SELECT文

--- mysql/source.sql
+++ mysql/source.sql
@@ -2,3 +2,11 @@ mysql -u root
 /* mysqlでshemaとdataを読み込む */
 source /home/ubuntu/bookstore-schema.sql
 source /home/ubuntu/bookstore-data.sql
+
+/* mysqlだけUSEコマンドがある*/
+ USE bookstore;
+
+/*SELECTでstaffを取得*/
+ SELECT * FROM staff;
+/*SELECTで必要なカラムだけ取得*/
+ SELECT book_id, title, year FROM book;
